8718232ea87c91994baca8cc956c89ecc8964954
kayce <justinross28@gmail.com>
2022-11-09 15:22:04 -0800

    new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,15 +19,35 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
-});
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  })
+    .then(data => res.json(data))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(data => res.json(data))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
