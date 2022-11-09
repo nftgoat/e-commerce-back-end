d6dfce4eb306ab83a2338f2775c45f5760ac5a62
kayce <justinross28@gmail.com>
2022-11-09 15:39:09 -0800

    finished product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -72,6 +72,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ message: 'ID does not match any category' });
+        return;
+      }
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
